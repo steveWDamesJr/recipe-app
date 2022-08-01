6779d5438cfbe1ce137c26d81e8e5b0b481450d9
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-08-01 09:40:24 -0500

    Add devise user model and CRUD functions or USER

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  def current_user
+    current_user
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password])
+  end
+
+  def after_sign_out_for(_resources)
+    redirect_to new_user_session
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # root to: "home#index"
+  devise_for :users
+  resources :users
+  root to: "home#index"
 end
