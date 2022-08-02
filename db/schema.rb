13684f121ceb22354375af16d554051b2818be43
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-08-02 10:16:21 -0500

    Fix merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2022_08_02_124243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,10 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_02_124243) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_08_02_073412) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  end
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
@@ -39,7 +35,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_02_073412) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
->>>>>>> origin/food-list
   end
 
   create_table "users", force: :cascade do |t|
@@ -60,9 +55,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_02_073412) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
   add_foreign_key "recipes", "users"
-=======
   add_foreign_key "foods", "users"
->>>>>>> origin/food-list
 end
