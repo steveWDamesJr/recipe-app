a43fcc8ea393a96773e74f0cc37f262fd2080dd7
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-02 16:03:03 +0100

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'users#index'
+
   resources :users do
     resources :foods, only: [:index, :new, :create, :destroy]
   end
-  root 'users#index'  
+
 end
