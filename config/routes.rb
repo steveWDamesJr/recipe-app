96e8ad6dcc4c1ed03bccd1679022354eac0a723c
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-03 15:23:16 +0100

    remove nested route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
 
-  resources :users do
-    resources :foods, only: [:index, :new, :create, :destroy]
-  end
+  resources :users
+  resources :foods, only: [:index, :new, :create, :destroy]
 
 end
