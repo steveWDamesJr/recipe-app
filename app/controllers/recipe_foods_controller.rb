20f93dbb5291c2596a236343b1849f766e723d21
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-04 12:13:47 +0100

    add controller methods for recipe foods

---
+++ app/controllers/recipe_foods_controller.rb
@@ -0,0 +1,43 @@
+class RecipeFoodsController < ApplicationController
+  before_action :set_recipe_food, only: %i[ destroy ]
+
+  # GET /recipe_foods/new
+  def new
+    @recipe_food = RecipeFood.new
+    @recipe = Recipe.find(params[:recipe_id])
+  end
+
+  # POST /recipe_foods or /recipe_foods.json
+  def create
+    @recipe_food = RecipeFood.new(recipe_food_params)
+    @recipe_food.recipe = Recipe.find(params[:recipe_id])
+
+    respond_to do |format|
+      if @recipe_food.save
+        format.html { redirect_to recipe_food_url(@recipe_food), notice: "Recipe food was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /recipe_foods/1 or /recipe_foods/1.json
+  def destroy
+    @recipe_food.destroy
+
+    respond_to do |format|
+      format.html { redirect_to recipe_foods_url, notice: "Recipe food was successfully destroyed." }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_recipe_food
+      @recipe_food = RecipeFood.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def recipe_food_params
+      params.require(:recipe_food).permit(:food_id, :quantity)
+    end
+end
