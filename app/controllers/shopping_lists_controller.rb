6fd71e1883dac0638015a4bcc4b98536b339d3f3
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-05 23:29:51 +0100

    shopping list controller

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -1,3 +1,15 @@
 class ShoppingListsController < ApplicationController
   
+  def index
+    @shopping_lists = Food.find_by_sql(
+      "SELECT foods.name, foods.price, foods.quantity, foods.measurement_unit
+      FROM foods INNER JOIN recipe_foods ON foods.id = recipe_foods.food_id
+      WHERE foods.user_id = #{current_user.id}"
+    )
+    @total_cost = Food.find_by_sql(
+      "SELECT SUM(foods.price) AS total_cost FROM foods
+      INNER JOIN recipe_foods ON foods.id = recipe_foods.food_id
+      WHERE foods.user_id = #{current_user.id}"
+    )
+  end
 end
