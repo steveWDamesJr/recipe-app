683b41ce137d2fce34f643776e5004245f2e2b80
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-05 10:05:31 +0100

    fix linter errors

--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe RecipeFood, type: :model do
   before :each do
     @user = User.create(name: 'John Doe', email: 'test@mail.com', password: 'password')
     @food = Food.create(name: 'Pizza', measurement_unit: 'g', price: 10, quantity: 100, user: @user)
-    @recipe = Recipe.create(name: 'Pizza', preparation_time: 10, cooking_time: 20, description: 'Pizza', public: true, user: @user)
+    @recipe = Recipe.create(name: 'Pizza', preparation_time: 10, cooking_time: 20, description: 'Pizza', public: true,
+                            user: @user)
     @recipe_food = RecipeFood.create(quantity: 100, food: @food, recipe: @recipe)
   end
 
