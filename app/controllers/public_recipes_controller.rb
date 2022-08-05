f5de4bc73451ff2f2c6f731e51ba5b551193469f
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-08-05 23:29:38 +0100

    public recipe controller

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,4 +1,7 @@
 class PublicRecipesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index]
   
+  def index
+    @recipes = Recipes.includes(:user).where(public: true)
+  end
 end
