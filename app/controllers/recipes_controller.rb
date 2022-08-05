ed67fdf0c2ebc182b65473110bc1d88df41024af
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-08-05 03:59:39 -0500

    Fix linter errors

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -23,7 +23,7 @@ class RecipesController < ApplicationController
   def update
     respond_to do |format|
       if @recipe.update(recipe_params)
-        format.html { redirect_to recipe_url(@recipe), notice: "Recipe was successfully updated." }
+        format.html { redirect_to recipe_url(@recipe), notice: 'Recipe was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
